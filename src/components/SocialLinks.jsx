56e67ff090bdc8dba88e055a0c1e627e8ca5877c
Hamza Sahi <muh.hamza.sahii@gmail.com>
2026-03-17 10:12:50 +0500

    Resume file updated in portfolio

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -40,7 +40,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "./Hamza's-CV.pdf",
+      href: "./Resume.pdf",
       style: "rounded-br-md",
       download: true,
     },
